6bead561414b6bb1e31ea68c91e3cab7ac089dc2
Sergey K <kurservlad@yandex.ru>
2024-12-13 17:59:21 +0300

    Добавлено поле status для модели Order. Настроен orders/admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from orders.models import Order
+from django.forms import ModelForm
 
-# Register your models here.
+
+class OrderAdminForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = ['customer', 'robot_serial']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'robot_serial', 'status']
+    form = OrderAdminForm
--- orders/models.py
+++ orders/models.py
@@ -4,5 +4,11 @@ from customers.models import Customer
 
 
 class Order(models.Model):
+    STATUS_CHOICES = [
+        ('1', 'pending'),
+        ('2', 'completed'),
+    ]
+
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
     robot_serial = models.CharField(max_length=5,blank=False, null=False)
+    status = models.CharField(max_length=20, default='1', choices=STATUS_CHOICES)
